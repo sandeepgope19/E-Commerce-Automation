67c816a88ca7cf1627282f227545cdc3c51a2460
Sandeep Gope <sandeepgope19@gmail.com>
2026-04-12 15:33:53 +0530

    Initial commit - Selenium automation framework

---
+++ src/test/java/testBase/BaseClass.java
@@ -0,0 +1,70 @@
+package testBase;
+
+import java.time.Duration;
+
+//import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.apache.commons.text.RandomStringGenerator;
+
+public class BaseClass {
+
+	public WebDriver driver;
+
+	@BeforeClass
+	public void setup()
+	{
+		driver=new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+	public void tearDown()
+	{
+		driver.close();
+	}
+
+
+	//	public String randomString()
+	//	{
+	//		String generatedString=RandomStringUtils.randomAlphabetic(5);
+	//		return generatedString;
+	//	}
+	//
+	//	public String randomNumber()
+	//	{
+	//		String generatedString=RandomStringUtils.randomNumeric(10);
+	//		return generatedString;
+	//	}
+	//
+	//	public String randomAlphaNumeric()
+	//	{
+	//		String str=RandomStringUtils.randomAlphabetic(3);
+	//		String num=RandomStringUtils.randomNumeric(3);
+	//
+	//		return (str+"@"+num);
+	//	}
+
+	public String randomString() {
+		RandomStringGenerator generator = new RandomStringGenerator.Builder().withinRange('a', 'z').build();
+		return generator.generate(5);
+	}
+
+	public String randomNumber() {
+		RandomStringGenerator generator = new RandomStringGenerator.Builder().withinRange('0', '9').build();
+		return generator.generate(10);
+	}
+
+	public String randomAlphaNumeric() {
+		RandomStringGenerator alphaGen = new RandomStringGenerator.Builder().withinRange('a', 'z').build();
+		RandomStringGenerator numGen = new RandomStringGenerator.Builder().withinRange('0', '9').build();
+		return alphaGen.generate(3) + "@" + numGen.generate(3);
+	}
+	
+}
