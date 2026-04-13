045841e9088d83bfe404aa6c141fe12cb984ad11
Sandeep Gope <sandeepgope19@gmail.com>
2026-04-13 11:42:29 +0530

    Added properties file to ream common data.

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,6 +1,9 @@
 package testBase;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.commons.text.RandomStringGenerator;
 import org.openqa.selenium.WebDriver;
@@ -16,41 +19,49 @@ public class BaseClass {
 
 	public WebDriver driver;
 	public Logger logger;
-	
-	
+	public Properties p;
+
+
 	@BeforeClass
 	@Parameters({"os", "browser"})
-	public void setup(String os, String br)
-	
+	public void setup(String os, String br) throws IOException
+
 	{
-	
+
+		//loading properties file
+		FileReader file=new FileReader(".//src//test//resources//config.properties");
+		p=new Properties();
+		p.load(file);
+
+		//loading log4j file
 		logger=LogManager.getLogger(this.getClass());//Log4j
-		
+
+		//launching browser based on condition
 		switch(br.toLowerCase())
 		{
 		case "chrome": driver=new ChromeDriver(); 
 		break;
-		
+
 		case "edge": driver=new EdgeDriver(); 
 		break;
-		
+
 		default: System.out.println("No matching browser..");
 		return;
 		}
-		
+
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		driver.get("https://tutorialsninja.com/demo/");
+
+		driver.get(p.getProperty("appURL"));
 		driver.manage().window().maximize();
 	}
-	
+
 	@AfterClass
 	public void tearDown()
 	{
 		driver.close();
 	}
-	
+
 
 	public String randomString() {
 		RandomStringGenerator generator = new RandomStringGenerator.Builder().withinRange('a', 'z').build();
